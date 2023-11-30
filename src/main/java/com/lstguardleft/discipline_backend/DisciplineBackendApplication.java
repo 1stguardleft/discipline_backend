89fd075a36860455959bdb684180c0963588ccb4
lstguardleft <lstguardleft@gmail.com>
2023-11-30 13:21:16 +0900

    initial commit

---
+++ src/main/java/com/lstguardleft/discipline_backend/DisciplineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lstguardleft.discipline_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lstguardleft/discipline_backend/DisciplineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lstguardleft.discipline_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisciplineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
